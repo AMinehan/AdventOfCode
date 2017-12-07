072844ea4a4a1e35d0b3605c0002485de0cb405d
AMinehan <soloin@gmail.com>
2017-12-06 17:13:07 -0800

    day 6 initial commit

---
+++ day6.js
@@ -0,0 +1,13 @@
+function partOne(arr){
+
+}
+
+function partTwo(arr){
+
+}
+
+
+var input = [10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 3, 6];
+
+console.log(partOne(input));
+console.log(partTwo(input));
